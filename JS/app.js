3107a2d2616c1c253afa47e1513411dbf9e034e7
mdemon899 <mdnorealomemon@gmail.com>
2024-05-08 21:56:22 +0600

    Full Development Done

--- JS/app.js
+++ JS/app.js
@@ -1,2 +1,9 @@
 let eidAudio = document.getElementById("eidAudio");
-eidAudio.volume = 0.2;
+eidAudio.volume = 0.3;
+
+let receiver = document.getElementById("receiver");
+let sender = document.getElementById("sender");
+
+receiver.innerHTML = new URLSearchParams(location.search).get("receiver");
+sender.innerHTML =
+  new URLSearchParams(location.search).get("sender") || "Sakib Hassan";
